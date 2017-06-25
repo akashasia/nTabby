4b00517f3267a0a88f7790b863d427a09d869ff3
Akash <akashasia@gmail.com>
2017-06-24 23:32:07 -0400

    Add apps page

--- myApps.js
+++ myApps.js
@@ -5,42 +5,42 @@
 // 	document.getElementById("date").textContent = date.slice(0,-4);
 // }
 
- document.addEventListener("DOMContentLoaded", function() {
-        chrome.management.getAll(getAllCallback);
-      });
+document.addEventListener("DOMContentLoaded", function () {
+  chrome.management.getAll(getAllCallback);
+});
 
-      var getAllCallback = function(list) {
-        var apps = document.getElementById("apps");
-  for(var i in list) {
+var getAllCallback = function (list) {
+  var apps = document.getElementById("apps");
+  for (var i in list) {
     // we don't want to do anything with extensions yet.
     var extInf = list[i];
-    if(extInf.isApp && extInf.enabled) {
+    if (extInf.isApp && extInf.enabled) {
       var app = document.createElement("div");
 
       var img = new Image();
       img.className = "image";
       img.src = find128Image(extInf.icons);
 
-      img.addEventListener("click", (function(ext) {
-    return function() {
-        chrome.management.launchApp(ext.id);
-    };
-})(extInf));
+      app.addEventListener("click", (function (ext) {
+        return function () {
+          chrome.management.launchApp(ext.id);
+        };
+      })(extInf));
 
       var name = document.createElement("div");
       name.className = "name";
       name.textContent = extInf.name;
 
-      app.className = "app";
+      app.className = "box";
       app.appendChild(img);
       app.appendChild(name);
       apps.appendChild(app);
     }
   }
-      };
-var find128Image = function(icons) {
-  for(var icon in icons) {
-    if(icons[icon].size == "128") {
+};
+var find128Image = function (icons) {
+  for (var icon in icons) {
+    if (icons[icon].size == "128") {
       return icons[icon].url;
     }
   }
